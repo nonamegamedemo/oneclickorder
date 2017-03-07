fc76527d108047f07109998f80d1c1628ad93237
Zero <zero@mobote.com>
2017-03-07 09:53:34 +0800

    新增：首页添加链接普通页面

--- src/pages/tours-recommend-container.jsx
+++ src/pages/tours-recommend-container.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router';
 
 export class ToursRecommendContainer extends Component {
     render() {
         return (
-            <div>Tours Recommend Container</div>
+            <div>
+                <h1>推荐游记</h1>
+                <p>
+                    <Link to="/tours">游记</Link>
+                </p>
+                <p>
+                    <Link to="/orders">订单列表</Link>
+                </p>
+            </div>
         );
     }
 }
