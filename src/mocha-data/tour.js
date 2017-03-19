ee159978f8b93394161310034f4b1819dc449863
tracy <287033963@qq.com>
2017-03-19 23:03:11 +0800

    task: 更新游记列表页

---
+++ src/mocha-data/tour.js
@@ -0,0 +1,62 @@
+module.exports={
+	"tourList": [
+		{
+			"tourPassId": "1",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "暖冬出逃香港两日【一个人闲逛记】【中环 | 桥咀岛】",
+			"author": "猫猫児cathy",
+			"publicTime": "2017-01-23 17:20:00",
+            "tourCity": "香港",
+            "content": "内容，超过两行展示...，TODO",
+            "detailURL": ""
+		},
+		{
+			"tourPassId": "2",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "【兔美酱在路上】两只老友窥探【澳】秘，千千【港】乐你唱我听。",
+			"author": "雯哥is兔美酱",
+			"publicTime": "2016-07-24 17:20:00",
+			"tourCity": "香港",
+			"content": "内容，超过两行展示...，TODO",
+			"detailURL": ""
+		}, {
+			"tourPassId": "3",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "阴雨的香港，我们只为漫威而来",
+			"author": "蝈蝈小姐",
+			"publicTime": "2017-03-07 17:20:00",
+			"tourCity": "香港",
+			"content": "内容，超过两行展示...，TODO",
+			"detailURL": ""
+		},
+		{
+			"tourPassId": "4",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "暖冬出逃香港两日【一个人闲逛记】【中环 | 桥咀岛】",
+			"author": "猫猫児cathy",
+			"publicTime": "2017-01-23 17:20:00",
+            "tourCity": "香港",
+            "content": "内容，超过两行展示...，TODO",
+            "detailURL": ""
+		},
+		{
+			"tourPassId": "5",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "【兔美酱在路上】两只老友窥探【澳】秘，千千【港】乐你唱我听。",
+			"author": "雯哥is兔美酱",
+			"publicTime": "2016-07-24 17:20:00",
+			"tourCity": "香港",
+			"content": "内容，超过两行展示...，TODO",
+			"detailURL": ""
+		}, {
+			"tourPassId": "6",
+			"imgURL": "https://dimg01.c-ctrip.com/images/100a0c000000629plEE92_C_210_140.jpg",
+			"title": "阴雨的香港，我们只为漫威而来",
+			"author": "蝈蝈小姐",
+			"publicTime": "2017-03-07 17:20:00",
+			"tourCity": "香港",
+			"content": "内容，超过两行展示...，TODO",
+			"detailURL": ""
+		},
+	]
+}
--- src/pages/tours-container.jsx
+++ src/pages/tours-container.jsx
@@ -1,9 +1,92 @@
 import React, { Component } from 'react';
+import tourListData from '../mocha-data/tour.js';
+
 
 export class ToursContainer extends Component {
     render() {
+    	const listData = tourListData.tourList;
         return (
-            <div>tours container</div>
+            <CardList listData={listData}/>
         );
     }
 }
+
+class CardList extends Component {
+	_handleClick() {
+		alert('go to detail page');
+	}
+
+	render() {
+
+		const card = (
+			<div >
+				{this.props.listData.map((data) => 
+					<div style={cardStyle} key={data.tourPassId}>
+						<div style={{position: "relative"}}>
+							<span style={cityLabelStyle}>{data.tourCity}</span>
+							<img style={imgStyle} src={data.imgURL} />
+						</div>
+						<div>
+							<div>
+								<h1 onClick={this._handleClick}>{data.title}</h1>
+							</div>
+							<div>
+								<p>{data.author}</p>
+								<p>{data.publicTime}</p>
+							</div>
+							<div>
+								<p onClick={this._handleClick}>{data.content}</p>
+							</div>
+						</div>
+					</div>
+				)}
+			</div>
+		);
+
+		return (
+			<div>
+				{card}
+			</div>
+		);
+	}
+}
+
+const floatStyle = {
+	float: "left"
+}
+
+const cardStyle = {
+	width: "100%",
+	border: "1px solid black",
+	margin: "10px",
+};
+
+const imgStyle = {
+	width: "210px",
+	height: "140px"
+};
+
+const cityLabelStyle = {
+	position: "absolute",
+	top: "0",
+	left: "0",
+	height: "20px",
+	background: "rgba(102,102,102,0.9)",
+	font: "12px",
+	color: "white",
+	linehight: "12px",
+	padding: "0px 6px 0px 8px"
+};
+
+/*const cityLabelAfterStyle = {
+	position: "absolute",
+	right: "-4px",
+	top: "0px",
+	width: "0px",
+	height: "0px",
+	border: "10px solid rgba(102,102,102,0.9)",
+	border-color: "rgba(102,102,102,0.9) transparent transparent rgba(102,102,102,0.9);",
+	    border-left-width: "2px";
+	    border-right-width: "2px";
+
+};*/
