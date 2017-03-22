a3da7ba654a05b44baddbcee6dbbc2db85a0fb9e
wury <wury@Ctrip.com>
2017-03-22 17:47:20 +0800

    新增游记详情页，修改gitignore加入游记静态页面数据

---
+++ src/mocha-data/tour-schedule.js
@@ -0,0 +1,202 @@
+module.exports = {
+    "tourScheduleList": {
+        "3386248": {
+            "totalDayCount":12,
+            "budget":12000,
+            "with":"夫妻",
+            "scheduleList":[
+                [
+                    {
+                        "type":"flight",
+                        "timeQuantum": "pm",
+                        "from": "上海虹桥国际机场T1",
+                        "to": "莫斯科国际机场"
+                    },{
+                        "type":"flight",
+                        "timeQuantum": "pm",
+                        "from": "莫斯科国际机场",
+                        "to": "马拉加黄金海岸机场"
+                    },{
+                        "type":"hotel",
+                        "name": "santa isabel la real"
+                    }
+                ],
+                [
+                    {
+                        "type":"viewspot",
+                        "place": "阿尔罕布拉宫"
+                    },{
+                        "type":"viewspot",
+                        "place": "阿尔拜辛"
+                    },{
+                        "type":"viewspot",
+                        "place": "圣尼古拉斯眺望台"
+                    },{
+                        "type":"hotel",
+                        "name": "santa isabel la real"
+                    }
+                ],
+                [
+                    {
+                        "type":"train",
+                        "timeQuantum": "am",
+                        "from": "格拉纳达火车站",
+                        "to": "龙达火车站"
+                    },{
+                        "type":"viewspot",
+                        "place": "龙达斗牛场"
+                    },{
+                        "type":"viewspot",
+                        "place": "龙达新桥"
+                    },{
+                        "type":"hotel",
+                        "name": "hotel montelirio"
+                    }
+                ],
+                [
+                    {
+                        "type":"car",
+                        "timeQuantum": "am",
+                        "from": "龙达长途汽车站",
+                        "to": "塞维利亚长途汽车站"
+                    },{
+                        "type":"viewspot",
+                        "place": "塞维利亚王宫"
+                    },{
+                        "type":"viewspot",
+                        "place": "塞维利亚圣母主教座堂"
+                    },{
+                        "type":"viewspot",
+                        "place": "弗拉门戈表演"
+                    },{
+                        "type":"hotel",
+                        "name": "H10 cORREGIDOR Boutique Hotel"
+                    }
+                ],
+                [
+                    {
+                        "type":"train",
+                        "timeQuantum": "am",
+                        "from": "塞维利亚火车站",
+                        "to": "科尔多瓦火车站"
+                    },{
+                        "type":"viewspot",
+                        "place": "犹太教堂"
+                    },{
+                        "type":"viewspot",
+                        "place": "科尔多瓦古罗马桥"
+                    },{
+                        "type":"viewspot",
+                        "place": "科尔多瓦大清真寺百花巷"
+                    },{
+                        "type":"hotel",
+                        "name": "Cordoba Carpe Diem"
+                    }
+                ],
+                [
+                    {
+                        "type":"train",
+                        "timeQuantum": "am",
+                        "from": "科尔多瓦火车站",
+                        "to": "阿托查火车站"
+                    },{
+                        "type":"viewspot",
+                        "place": "塞尔维亚阿尔卡萨"
+                    },{
+                        "type":"viewspot",
+                        "place": "罗马大渡槽"
+                    },{
+                        "type":"hotel",
+                        "name": "Hostal Met Madrid"
+                    }
+                ],
+                [
+                    {
+                        "type":"viewspot",
+                        "place": "格兰大道"
+                    },{
+                        "type":"viewspot",
+                        "place": "伯纳乌球场"
+                    },{
+                        "type":"hotel",
+                        "name": "Hostal Met Madrid"
+                    }
+                ],
+                [
+                    {
+                        "type":"flight",
+                        "timeQuantum": "am",
+                        "from": "马德里",
+                        "to": "巴塞罗那"
+                    },{
+                        "type":"viewspot",
+                        "place": "兰布拉大道"
+                    },{
+                        "type":"viewspot",
+                        "place": "波盖利亚市场"
+                    },{
+                        "type":"hotel",
+                        "name": "Atlantis by Atbcn"
+                    }
+                ],
+                [
+                    {
+                        "type":"car",
+                        "timeQuantum": "am",
+                        "from": "巴塞罗那",
+                        "to": "海滨托萨"
+                    },{
+                        "type":"hotel",
+                        "name": "Atlantis by Atbcn"
+                    }
+                ],
+                [
+                    {
+                        "type":"viewspot",
+                        "place": "巴塞罗那圣家堂"
+                    },{
+                        "type":"viewspot",
+                        "place": "米拉之家"
+                    }，{
+                        "type":"viewspot",
+                        "place": "巴特罗之家"
+                    },{
+                        "type":"hotel",
+                        "name": "Atlantis by Atbcn"
+                    }
+                ],
+                [
+                    {
+                        "type":"viewspot",
+                        "place": "诺坎普球场"
+                    },{
+                        "type":"viewspot",
+                        "place": "古埃尔公园"
+                    }，{
+                        "type":"viewspot",
+                        "place": "加泰罗尼亚广场"
+                    }，{
+                        "type":"viewspot",
+                        "place": "哥特区"
+                    },{
+                        "type":"hotel",
+                        "name": "Atlantis by Atbcn"
+                    }
+                ],
+                [
+                    {
+                        "type":"flight",
+                        "timeQuantum": "am",
+                        "from": "巴塞罗那",
+                        "to": "莫斯科"
+                    },{
+                        "type":"flight",
+                        "timeQuantum": "pm",
+                        "from": "莫斯科",
+                        "to": "上海"
+                    }
+                ]
+            ]
+        }
+    }
+}
--- src/pages/tour-container.jsx
+++ src/pages/tour-container.jsx
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
-
 export class TourContainer extends Component {
     render() {
-        return (
-            <div>tour container {this.props.params.tourid}</div>
-        );
+        let tourId = this.props.params.tourid;
+        let url = "/tour-common/"+tourId+"/"+tourId+".html"
+        return <div><button style={buttonStyle} className="ui orange button">一键下单</button><iframe style={iframeStyle} src={url}></iframe></div>
     }
+}
+
+const iframeStyle = {
+	width:"100%",
+    height:"1000px",
+    top:"0px",
+    position:"absolute"
+}
+
+const buttonStyle = {
+    top:"100px",
+    right:"50px",
+    position:"absolute",
+    zIndex:"1000"
 }
