cd9028582e0c040bcb8f097518757375503ad4bf
Zero <zero@mobote.com>
2017-03-23 22:32:26 +0800

    更新数据

--- src/mocha-data/order.js
+++ src/mocha-data/order.js
@@ -5,6 +5,80 @@ module.exports = {
             "from": "Shanghai",
             "to": "Australia",
             "tour": "1",
+            "destinations": [
+                {
+                    "id": "1",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Sydney Opera House",
+                    "address": "No2 street",
+                    "date": "2017-08-17"
+                },
+                {
+                    "id": "2",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Sydney Harbour Bridge",
+                    "address": "No2 street",
+                    "date": "2017-08-17"
+                },
+                {
+                    "id": "3",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Sydney Harbour Bridge",
+                    "address": "No2 street",
+                    "date": "2017-08-18"
+                },
+                {
+                    "id": "4",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Darling Harbour",
+                    "address": "No2 street",
+                    "date": "2017-08-19"
+                },
+                {
+                    "id": "5",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Taronga Zoo",
+                    "address": "No2 street",
+                    "date": "2017-08-20"
+                },
+                {
+                    "id": "6",
+                    "type": "dest",
+                    "city": "Melbourne",
+                    "name": "Great Ocean Road",
+                    "address": "Great Ocean Road",
+                    "date": "2017-08-21"
+                },
+                {
+                    "id": "7",
+                    "type": "dest",
+                    "city": "Melbourne",
+                    "name": "Melbourne Cricket Ground",
+                    "address": "Melbourne Cricket Ground",
+                    "date": "2017-08-22"
+                },
+                {
+                    "id": "8",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Bondi Beach",
+                    "address": "No2 street",
+                    "date": "2017-08-23"
+                },
+                {
+                    "id": "9",
+                    "type": "dest",
+                    "city": "Sydney",
+                    "name": "Sydney Tower",
+                    "address": "No2 street",
+                    "date": "2017-08-24"
+                },
+            ],
             "flight": [
                 {
                     "id": "1",
@@ -37,7 +111,7 @@ module.exports = {
                     "from": "Sydney",
                     "to": "Melbourne",
                     "departure": "2017-08-20 12:20:00",
-                    "arrive": "2017-08-21 15:20:00",
+                    "arrive": "2017-08-20 15:20:00",
                     "departureTerminal": "Sydney Train Station",
                     "arrivalTerminal": "Melbourne Train Station"
                 },
@@ -74,6 +148,7 @@ module.exports = {
                 },
                 {
                     "id": "3",
+                    "type": "hotel",
                     "hotel": "Sydney Mock Hotel2",
                     "city": "Sydney",
                     "address": "No2 street",
