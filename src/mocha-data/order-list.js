ec8290e110eb3012070145bf45ba38ae5f7c9002
denghuang <denghuang@Ctrip.com>
2017-03-24 14:40:51 +0800

    modify order-list and orders container

--- src/mocha-data/order-list.js
+++ src/mocha-data/order-list.js
@@ -2,44 +2,26 @@ module.exports = {
     "orderList": [
          
         {
-            "orderNumberDes": "订单号： ",
-            "orderDateDes": "预定日期： ",
-            "orderDetailsDes" : "订单详情",
-            "orderOption": "继续提交",
-            "orderId": 3315025269,
+            "orderOption": "点评",
+            "orderOptionUrl": "http://piao.ctrip.com/Order/Comment/3106245046?RepeatAction=Piao_Repeat_Comment",
+            "orderId": 1,
             "orderTitle": "长白山4日自由行",
+            "from": "Shanghai",
+            "to": "Australia",
             "orderDate": "2017-08-16 21:12:00",
             "orderBizType": "旅游度假",
             "orderStatus":  "未提交",
             "orderPrice": "5000",
             "travelBeginTime": "2017-08-16 21:12:00",
-            "travelEndTime": "2017-08-20 21:12:00",
+            "travelEndTime": "2017-08-25 08:30:00",
             "passengerNames": "黎明，刘海",
             "orderDetailsUrl": "http://piao.ctrip.com/Order/Detail/3106245046?RepeatAction=Piao_Repeat_OrderDetail",
             "orderTitleUrl": "http://taocan.ctrip.com/freetravel/p77011s2.html?RepeatAction=Vacation_Repeat_PkgDetail",
             "orderOptionUrl": "http://taocan.ctrip.com/freetravel/p77011s2.html?RepeatAction=Vacation_Repeat_PkgDetail",
-            "currency" : "¥",
-            "uid": "E201365"
+            "currency" : "¥ ",
            
         },
-        {
-            "orderNumberDes": "订单号： ",
-            "orderDateDes": "预定日期： ",
-            "orderDetailsDes" : "订单详情",
-            "orderId": 3385025270,
-            "orderTitle": "马来西亚5日自由行",
-            "orderDate": "2017-08-16 21:12:00",
-            "orderBizType": "旅游度假",
-            "orderStatus":  "未提交",
-            "orderPrice": "5000",
-            "travelBeginTime": "2017-08-16 21:12:00",
-            "travelEndTime": "2017-08-20 21:12:00",
-            "passengerNames": "黎明，刘海",
-            "orderDetailsUrl": "http://piao.ctrip.com/Order/Detail/3106245046?RepeatAction=Piao_Repeat_OrderDetail",
-            "orderTitleUrl": "http://vacations.ctrip.com/grouptravel/p3091968s2.html",
-            "currency" : "¥",
-            "uid": "E201365"           
-        }
+       
     ]
 
 
